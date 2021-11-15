9d22bfc6b5ba669c70df8272b0cce68c1520ad16
Zach <zachulibarri@gmail.com>
2021-11-15 10:58:32 -0800

    added comment to FSFruitstore.js

--- cypress/integration/FSFruitStore/FSFruitStore.js
+++ cypress/integration/FSFruitStore/FSFruitStore.js
@@ -1,13 +1,8 @@
 /*
-describe('FS Events Suite', () => {
-  
-  
-  it('Navigates to the Fruit Shoppe cart', () => {
-    cy.visit('https://fruitshoppe.firebaseapp.com/?fullstory=prod ');
-    cy.wait(20000)
+This is left here as an example of how I should be able to spy on the call going out to the FS server, but I have been unable 
+validate it working due to the bundles not appearing in the Cypress browser.  
 
-
-     cy.intercept('POST', 'https://rs.fullstory.com/rec/bundle*').as('bundle')
+    cy.intercept('POST', 'https://rs.fullstory.com/rec/bundle*').as('bundle')
     cy.wait('@bundle', {timeout:10000}).then((req) => {
       expect(req.body).to.include('kind:13')
     })
